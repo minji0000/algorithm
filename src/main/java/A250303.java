12bf03e9b90387da5c3db1d5109a4b0b633685da
minji0000 <bluurmi@gmail.com>
2025-03-03 23:39:22 +0900

    동전 합쳐서 금액 만드는 경우의 수

---
+++ src/main/java/A250303.java
@@ -0,0 +1,37 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class A250303 {
+
+    public static int countCoinCases(int n, int k, int[] coins) {
+        int[] dp = new int[k + 1];
+        dp[0] = 1;  // 초기값 설정
+
+        for (int coin : coins) {
+            for (int j = coin; j <= k; j++) {
+                dp[j] += dp[j-coin];  // 현재 동전을 사용하는 경우의 수 추가
+            }
+        }
+
+        return dp[k];  // 최종 결과 반환
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(countCoinCases(n, k, coins));
+    }
+}
+
