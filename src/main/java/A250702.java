2e0e4fd9380c43997b8be77b63a6c1325c0fe2d8
minji0000 <bluurmi@gmail.com>
2025-07-02 23:56:41 +0900

    solving A250702

---
+++ src/main/java/A250702.java
@@ -0,0 +1,51 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A250702
+{
+
+        public static int maxPathSum(int n, int[][] triangle) {
+            // dp 배열 초기화
+            int[][] dp = new int[n][n];
+            dp[0][0] = triangle[0][0];
+
+            // 모든 위치에서 최대 합 계산
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    if (j == 0) {
+                        dp[i][j] = dp[i-1][j] + ____;  // 왼쪽 끝인 경우
+                    } else if (j == i) {
+                        dp[i][j] = dp[i-1][j-1] + ____;  // 오른쪽 끝인 경우
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + ____;  // 중간인 경우
+                    }
+                }
+            }
+
+            // 마지막 행에서 최대값 찾기
+            int maxSum = dp[n-1][0];
+            for (int j = 1; j < n; j++) {
+                maxSum = Math.max(maxSum, dp[n-1][j]);
+            }
+            return maxSum;
+        }
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int[][] triangle = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j <= i; j++) {
+                    triangle[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            System.out.println(maxPathSum(n, triangle));
+        }
+}
